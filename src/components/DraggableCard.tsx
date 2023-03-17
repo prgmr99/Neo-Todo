153ebc13837254770335c1e810b9941c2c85e8ca
prgmr99 <johnyeom24@naver.com>
2023-03-17 23:13:00 +0900

    [Mod] issue #14

--- src/components/DraggableCard.tsx
+++ src/components/DraggableCard.tsx
@@ -5,7 +5,7 @@ import { useSetRecoilState } from "recoil";
 import { Draggable } from "react-beautiful-dnd";
 import { toDoState, ITodo } from "../atoms";
 import DelBtn from "./DelBtn";
-import Specific from "./Specific";
+import Swal from "sweetalert2";
 
 interface IDraggableCardProps {
   toDoId: number;
@@ -33,14 +33,51 @@ const Card = styled.div<ICard>`
       ? "0px 3px 5px rgba(0,0,0,1)"
       : "0px 1px 2px rgba(0,0,0,1)"};
   font-size: 15px;
-  display: flex;
+
   color: ${(props) => props.theme.textColor};
   align-items: center;
   word-break: break-all;
 `;
+const ModBtn = styled.button`
+  border: none;
+  background-color: transparent;
+  color: white;
+  float: right;
+`;
 
 function DraggableCard({ toDoId, toDo, index, boardId }: IDraggableCardProps) {
   const setTodos = useSetRecoilState(toDoState);
+  const onFixBtn = (index: number) => {
+    (async () => {
+      const { value: getTodo } = await Swal.fire({
+        title: "새로운 Todo를 입력해주세요.",
+        text: "변경할 Todo",
+        input: "text",
+        inputPlaceholder: "Todo를 입력해주세요.",
+      });
+
+      // 이후 처리되는 내용.
+      if (getTodo) {
+        Swal.fire("Saved!");
+      }
+      if (getTodo === "") {
+        return;
+      }
+      setTodos((prev) => {
+        const toDosCopy = [...prev];
+        const boardIndex = prev.findIndex((b) => b.id === boardId);
+        const boardCopy = { ...toDosCopy[boardIndex] };
+        const listCopy = [...boardCopy.toDos];
+        const listIndex = listCopy.findIndex((b) => b.id === toDoId);
+        const listCCopy = { ...listCopy[listIndex] };
+        listCCopy.text = getTodo;
+        listCopy[listIndex] = listCCopy;
+        boardCopy.toDos = listCopy;
+        toDosCopy[boardIndex] = boardCopy;
+        return toDosCopy;
+      });
+    })();
+  };
   return (
     <div>
       <Draggable key={toDoId} draggableId={"todo-" + toDo.id} index={index}>
@@ -54,6 +91,7 @@ function DraggableCard({ toDoId, toDo, index, boardId }: IDraggableCardProps) {
           >
             <DelBtn index={index} toDoId={toDoId} boardId={boardId} />
             {toDo.text}
+            <ModBtn onClick={() => onFixBtn(index)}>M</ModBtn>
           </Card>
         )}
       </Draggable>

a3808b83f074c4e3e718fd193ec247d29ad7d385
prgmr99 <johnyeom24@naver.com>
2024-12-15 20:38:57 +0900

    refactor: #46 onDragEnd 함수 리팩토링

--- src/App.tsx
+++ src/App.tsx
@@ -81,82 +81,105 @@ function App() {
     })();
   };
 
+  const handleBoardReorder = (
+    sourceIndex: number,
+    destinationIndex: number
+  ) => {
+    setToDo((prev) => {
+      const toDosCopy = [...prev];
+      const [movedBoard] = toDosCopy.splice(sourceIndex, 1);
+      toDosCopy.splice(destinationIndex, 0, movedBoard);
+      return toDosCopy;
+    });
+  };
+
+  const handleToDoReorderWithinBoard = (
+    sourceIndex: number,
+    destinationIndex: number,
+    boardId: string
+  ) => {
+    setToDo((prev) => {
+      const toDosCopy = [...prev];
+      const boardIndex = toDosCopy.findIndex(
+        (board) => board.id + "" === boardId
+      );
+      const boardCopy = { ...toDosCopy[boardIndex] };
+      const [movedToDo] = boardCopy.toDos.splice(sourceIndex, 1);
+      boardCopy.toDos.splice(destinationIndex, 0, movedToDo);
+      toDosCopy[boardIndex] = boardCopy;
+      return toDosCopy;
+    });
+  };
+
+  const handleToDoMoveToAnotherBoard = (
+    sourceIndex: number,
+    destinationIndex: number,
+    sourceBoardId: string,
+    destinationBoardId: string
+  ) => {
+    setToDo((prev) => {
+      const toDosCopy = [...prev];
+      const sourceBoardIndex = toDosCopy.findIndex(
+        (board) => board.id + "" === sourceBoardId
+      );
+      const destinationBoardIndex = toDosCopy.findIndex(
+        (board) => board.id + "" === destinationBoardId
+      );
+
+      const sourceBoardCopy = { ...toDosCopy[sourceBoardIndex] };
+      const destinationBoardCopy = { ...toDosCopy[destinationBoardIndex] };
+
+      const [movedToDo] = sourceBoardCopy.toDos.splice(sourceIndex, 1);
+      destinationBoardCopy.toDos.splice(destinationIndex, 0, movedToDo);
+
+      toDosCopy[sourceBoardIndex] = sourceBoardCopy;
+      toDosCopy[destinationBoardIndex] = destinationBoardCopy;
+
+      return toDosCopy;
+    });
+  };
+
+  const handleToDoDelete = (sourceIndex: number, boardId: string) => {
+    setToDo((prev) => {
+      const toDosCopy = [...prev];
+      const boardIndex = toDosCopy.findIndex(
+        (board) => board.id + "" === boardId
+      );
+      const boardCopy = { ...toDosCopy[boardIndex] };
+      boardCopy.toDos.splice(sourceIndex, 1);
+      toDosCopy[boardIndex] = boardCopy;
+      return toDosCopy;
+    });
+  };
+
   const onDragEnd = (info: DropResult) => {
     const { destination, source } = info;
+
+    if (!destination) return;
+
+    const isSameBoard = source.droppableId === destination.droppableId;
+
     if (source.droppableId === "boards") {
-      if (!destination) return;
-      if (source.index === destination.index) return;
       if (source.index !== destination.index) {
-        setToDo((prev) => {
-          const toDosCopy = [...prev];
-          const prevBoard = toDosCopy[source.index];
-          toDosCopy.splice(source.index, 1);
-          toDosCopy.splice(destination.index, 0, prevBoard);
-          return toDosCopy;
-        });
+        handleBoardReorder(source.index, destination.index);
       }
-    } else if (source.droppableId !== "boards") {
-      if (!destination) return;
-      if (destination.droppableId === "trash") {
-        setToDo((prev) => {
-          const toDosCopy = [...prev];
-          const boardIndex = toDosCopy.findIndex(
-            (board) => board.id + "" === source.droppableId.split("-")[1]
-          );
-          const boardCopy = { ...toDosCopy[boardIndex] };
-          const listCopy = [...boardCopy.toDos];
-          listCopy.splice(source.index, 1);
-          boardCopy.toDos = listCopy;
-          toDosCopy.splice(boardIndex, 1, boardCopy);
-          return toDosCopy;
-        });
-        return;
+    } else if (destination.droppableId === "trash") {
+      handleToDoDelete(source.index, source.droppableId.split("-")[1]);
+    } else if (isSameBoard) {
+      if (source.index !== destination.index) {
+        handleToDoReorderWithinBoard(
+          source.index,
+          destination.index,
+          source.droppableId.split("-")[1]
+        );
       }
-      if (source.droppableId === destination.droppableId) {
-        setToDo((prev) => {
-          const toDosCopy = [...prev];
-          const boardIndex = toDosCopy.findIndex(
-            (board) => board.id + "" === source.droppableId.split("-")[1]
-          );
-          const boardCopy = { ...toDosCopy[boardIndex] };
-          const listCopy = [...boardCopy.toDos];
-          const prevToDo = boardCopy.toDos[source.index];
-          listCopy.splice(source.index, 1);
-          listCopy.splice(destination.index, 0, prevToDo);
-          boardCopy.toDos = listCopy;
-          toDosCopy.splice(boardIndex, 1, boardCopy);
-
-          return toDosCopy;
-        });
-      }
-      if (source.droppableId !== destination.droppableId) {
-        setToDo((prev) => {
-          const toDosCopy = [...prev];
-          const sourceBoardIndex = toDosCopy.findIndex(
-            (board) => board.id + "" === source.droppableId.split("-")[1]
-          );
-          const destinationBoardIndex = toDosCopy.findIndex(
-            (board) => board.id + "" === destination.droppableId.split("-")[1]
-          );
-          const sourceBoardCopy = { ...toDosCopy[sourceBoardIndex] };
-          const destinationBoardCopy = { ...toDosCopy[destinationBoardIndex] };
-          const sourceListCopy = [...sourceBoardCopy.toDos];
-          const destinationListCopy = [...destinationBoardCopy.toDos];
-
-          const prevToDo = sourceBoardCopy.toDos[source.index];
-
-          sourceListCopy.splice(source.index, 1);
-          destinationListCopy.splice(destination.index, 0, prevToDo);
-
-          sourceBoardCopy.toDos = sourceListCopy;
-          destinationBoardCopy.toDos = destinationListCopy;
-
-          toDosCopy.splice(sourceBoardIndex, 1, sourceBoardCopy);
-          toDosCopy.splice(destinationBoardIndex, 1, destinationBoardCopy);
-
-          return toDosCopy;
-        });
-      }
+    } else {
+      handleToDoMoveToAnotherBoard(
+        source.index,
+        destination.index,
+        source.droppableId.split("-")[1],
+        destination.droppableId.split("-")[1]
+      );
     }
   };
 
